9aee43624a62703589e526e52422e7e60b47e2c3
Familia Porcel <brandon7.7porcel@gmail.com>
2021-01-06 19:21:41 -0300

    con mas funciones, un poco mas ordenado

--- js/scripts.js
+++ js/scripts.js
@@ -3,29 +3,24 @@ const $canvas = d.getElementById('canvas');
 const $ctx = $canvas.getContext('2d');
 const $rangeInput = d.getElementById('range-input');
 const images = [];
-
+const renderImages = (index) => {
+	$ctx.drawImage(images[index], 0, 0, $canvas.width, $canvas.height);
+};
+const handleRangeInput = () => {
+	renderImages($rangeInput.value);
+};
 const getImages = () => {
 	for (let i = 1; i <= 36; i += 1) {
 		const number = `0${i}`.slice(-2);
 		const url = `https://stockx-360.imgix.net/Air-Jordan-1-Mid-Banned-2020/Images/Air-Jordan-1-Mid-Banned-2020/Lv2/img${number}.jpg?auto=format,compress&q=90&updated_at=1606326136&w=1000`;
 		const eachImage = new Image();
 		eachImage.src = url;
-		// meter un alt con banderas
-		// images.push(eachImage);
 		images[i] = eachImage;
 		if (i === 1) {
-			$ctx.drawImage(images[i], 0, 0, $canvas.width, $canvas.height);
+			renderImages(i);
 		}
 	}
 };
 
-$rangeInput.addEventListener('input', () => {
-	$ctx.drawImage(
-		images[$rangeInput.value],
-		0,
-		0,
-		$canvas.width,
-		$canvas.height
-	);
-});
+$rangeInput.addEventListener('input', handleRangeInput);
 d.addEventListener('DOMContentLoaded', getImages);
