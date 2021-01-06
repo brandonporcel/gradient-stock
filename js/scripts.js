2114d27607047dc0b2a913bb345781487da0272e
Familia Porcel <brandon7.7porcel@gmail.com>
2021-01-06 19:03:54 -0300

    pude renderizar una img pero se ve 140px

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+const d = document;
+const $canvas = d.getElementById('canvas');
+const $ctx = $canvas.getContext('2d');
+const $rangeInput = d.getElementById('range-input');
+const handleRangeInput = () => {
+	console.log($rangeInput.value);
+};
+const images = [];
+const renderImages = (index) => {
+	$ctx.drawImage(images[index], 0, 0, $canvas.width, $canvas.height);
+};
+const getImages = () => {
+	for (let i = 1; i <= 36; i += 1) {
+		const number = `0${i}`.slice(-2);
+		const url = `https://stockx-360.imgix.net/Air-Jordan-1-Mid-Banned-2020/Images/Air-Jordan-1-Mid-Banned-2020/Lv2/img${number}.jpg?auto=format,compress&q=90&updated_at=1606326136&w=1000`;
+		const eachImage = new Image();
+		eachImage.src = url;
+		// meter un alt con banderas
+		// images.push(eachImage);
+		images[i] = eachImage;
+		console.log(url);
+		if (i === 1) {
+			renderImages(i);
+		}
+	}
+};
+
+$rangeInput.addEventListener('input', handleRangeInput);
+d.addEventListener('DOMContentLoaded', getImages);
