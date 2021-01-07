42b92b4dc1bed60a3ef8aa8e28876c7100c68d11
Familia Porcel <brandon7.7porcel@gmail.com>
2021-01-07 13:14:58 -0300

    listo,tengo que pensar lo de las expresiones regulares

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 const d = document;
-// const $colorBtn = d.getElementById('add-color');
-// let checkColor = false;
+const $colorBtn = d.getElementById('add-color');
 const $canvas = d.getElementById('canvas');
 const $ctx = $canvas.getContext('2d');
 const $rangeInput = d.getElementById('range-input');
@@ -36,37 +35,41 @@ const getColorPalette = () => {
 const setProperties = (colors) => {
 	d.body.style.setProperty(
 		'--primary',
-		`rgb(${colors[0].red},${colors[0].green},${colors[0].blue})`
+		`rgb(${colors[50].red},${colors[50].green},${colors[50].blue})`
 	);
-	console.log(colors[2]);
+
 	d.body.style.setProperty(
 		'--secondary',
-		`rgb(${colors[1].red},${colors[1].green},${colors[1].blue})`
+		`rgb(${colors[100].red},${colors[100].green},${colors[100].blue})`
 	);
 	d.body.style.setProperty(
 		'--tertiary',
-		`rgb(${colors[2].red},${colors[2].green},${colors[2].blue})`
+		`rgb(${colors[300].red},${colors[300].green},${colors[300].blue})`
 	);
-	// d.body.style.setProperty('--secondary', 'black');
-	// d.body.style.setProperty('--tertiary', 'red');
 };
 const getImages = () => {
 	for (let i = 1; i <= 36; i += 1) {
 		const number = `0${i}`.slice(-2);
-		// const url = `https://stockx-360.imgix.net/Air-Jordan-1-Mid-Banned-2020/Images/Air-Jordan-1-Mid-Banned-2020/Lv2/img${number}.jpg?auto=format,compress&q=90&updated_at=1606326136&w=1000`;
-		const url = `https://stockx-360.imgix.net/Converse-One-Star-Ox-Tyler-The-Creator-Golf-Le-Fleur-Purple-Green/Images/Converse-One-Star-Ox-Tyler-The-Creator-Golf-Le-Fleur-Purple-Green/Lv2/img${number}.jpg?auto=format,compress&q=90&updated_at=1603481985&w=1000`;
+		const url = `https://stockx-360.imgix.net/Air-Jordan-1-Mid-Banned-2020/Images/Air-Jordan-1-Mid-Banned-2020/Lv2/img${number}.jpg?auto=format,compress&q=90&updated_at=1606326136&w=1000`;
+
 		const eachImage = new Image();
 		eachImage.src = url;
 		eachImage.crossOrigin = 'anonymous';
 		images[i] = eachImage;
 		if (i === 1) {
-			renderImages(i);
-			const colors = getColorPalette();
-			setProperties(colors);
+			images[1].addEventListener('load', () => {
+				renderImages(i);
+				const colors = getColorPalette();
+				setProperties(colors);
+			});
 		}
 	}
 };
 
 d.addEventListener('DOMContentLoaded', getImages);
 $rangeInput.addEventListener('input', handleRangeInput);
-// $colorBtn.addEventListener('click', addingColor);
+$colorBtn.addEventListener('click', () => {
+	d.querySelector('.gradient').classList.toggle('opacity-on');
+	d.querySelector('.palette').classList.toggle('opacity-on');
+	$colorBtn.classList.toggle('color-black');
+});
