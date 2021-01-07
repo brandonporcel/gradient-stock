64050a0ff1a47b92f807364649e01da5fdea34e7
Familia Porcel <brandon7.7porcel@gmail.com>
2021-01-07 12:16:40 -0300

    pude poner color en la paleta,pero tengo problemas con el fondo

--- js/scripts.js
+++ js/scripts.js
@@ -11,44 +11,38 @@ const renderImages = (index) => {
 const handleRangeInput = () => {
 	renderImages($rangeInput.value);
 };
-function actualizarPropiedades(colors) {
-	console.log(colors[0]);
-	// console.log(colors[0].red);
-	d.body.style.setProperty(
-		'--primary',
-		`rgb(${colors[0].red},${colors[0].green},${colors[0].blue})`
-	);
-	d.body.style.setProperty(
-		'--secondary',
-		`rgb(${colors[1].red},${colors[1].green},${colors[1].blue})`
-	);
-	d.body.style.setProperty(
-		'--tertiary',
-		`rgb(${colors[2].red},${colors[2].green},${colors[2].blue})`
-	);
-}
 const getColorPalette = () => {
 	// Uint8ClampedArray depende del tamaño del canvas,cuando ma grande,mayou la cantidad
 	// data: Uint8ClampedArray(1744896)
 	// (canvas.width*canvas.height) *4(colores RGBA)
 	const imageData = $ctx.getImageData(0, 0, $canvas.width, $canvas.height).data;
 	// para no tener pixeles muy cercanos
-	const iterations = 100;
+	const iterations = 115;
 	const colors = [];
 	for (let i = 0; i < $canvas.width * $canvas.height; i += iterations) {
-		const iteratorElement = i * 3;
+		const iteratorElement = i * 4;
 		const colorRGB = imageData[iteratorElement];
 		if (colorRGB !== 0) {
-			if (colorRGB !== 255) {
+			if (colorRGB !== 255 && colorRGB < 220) {
 				const red = imageData[iteratorElement];
 				const green = imageData[iteratorElement + 1];
 				const blue = imageData[iteratorElement + 2];
 				colors.push({ red, green, blue });
-				// console.log('%c color', `background: rgba(${red}, ${green}, ${blue})`);
 			}
 		}
-		return colors;
 	}
+	return colors;
+};
+const setProperties = (colors) => {
+	d.body.style.setProperty(
+		'--primary',
+		`rgb(${colors[0].red},${colors[0].green},${colors[0].blue})`
+	);
+	d.body.style.setProperty(
+		'--secondary',
+		`rgb(${colors[1].red},${colors[1].green},${colors[1].blue}`
+	);
+	d.body.style.setProperty('--tertiary', 'red');
 };
 const getImages = () => {
 	for (let i = 1; i <= 36; i += 1) {
@@ -62,14 +56,11 @@ const getImages = () => {
 		if (i === 1) {
 			renderImages(i);
 			const colors = getColorPalette();
-			actualizarPropiedades(colors);
+			setProperties(colors);
 		}
 	}
 };
 
-// const addingColor = () => {
-// 	d.body.style.setProperty('--primary', 'red');
-// };
 d.addEventListener('DOMContentLoaded', getImages);
 $rangeInput.addEventListener('input', handleRangeInput);
 // $colorBtn.addEventListener('click', addingColor);
